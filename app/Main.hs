c0e00cac6db4d328f7e04208bf4a3688a87cacd5
Ming <146130+ming-t18@users.noreply.github.com>
2025-07-26 15:46:04 -0700

    refactor: Empty out Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,3 @@
 module Main (main) where
-
-import Lib (Conway, omega, mono1, veb1, epsilon, epsilon0, Ordinal)
-import FundamentalSeq(fsOrd)
-import Dyadic(Dyadic)
-import Numeric.Natural(Natural)
-
-test1, test2 :: Conway Dyadic
-test1 = mono1 (mono1 (veb1 omega 1 + 1) + 1)
-test2 = mono1 (veb1 5 0 + 1)
-
 main :: IO ()
-main = do
-    print (omega :: Conway Dyadic)
+main = return ()
