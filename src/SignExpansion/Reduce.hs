d70ebbb93b891e0c2c7163ebaa9d8a3ddc5af802
Ming <146130+ming-t18@users.noreply.github.com>
2025-08-19 22:25:26 -0700

    refactor: More readable "longest common prefix" logic

--- src/SignExpansion/Reduce.hs
+++ src/SignExpansion/Reduce.hs
@@ -1,6 +1,7 @@
 module SignExpansion.Reduce (reduce, reduceSingle) where
 
-import Control.Arrow ((&&&))
+import Data.Foldable (maximumBy)
+import Data.Ord (comparing)
 import Data.Set (Set)
 import qualified Data.Set as S
 import SignExpansion.Types as SE
@@ -15,8 +16,11 @@ reduce = snd . foldl reduceStep (S.empty, [])
     reduceStep :: (Set SignExpansion, [SignExpansion]) -> SignExpansion -> (Set SignExpansion, [SignExpansion])
     reduceStep (ps, pos) p = (S.insert p ps, pos ++ [po])
       where
-        pChosen = fmap (snd . fst) $ S.maxView $ S.map ((SE.length . commonPrefix p) &&& id) ps
-        po = maybe p (`reduceSingle` p) pChosen
+        -- Choose a previous @p@ by longest common prefix.
+        pChosen = maximumBy (comparing $ SE.length . commonPrefix p) ps
+        -- The reduced sign expansion
+        po = if null ps then p else pChosen `reduceSingle` p
 
+-- | Constructs a reduced sign expansion of the second argument given first argument as a base.
 reduceSingle :: SignExpansion -> SignExpansion -> SignExpansion
 reduceSingle po p = plus (SE.countSigns True pre) +++ suffix where (pre, (_, suffix)) = takeCommonPrefix po p
