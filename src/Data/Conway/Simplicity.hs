19f3f89a9f323aa11d7c17eb55f7c4f52e7ebd03
Ming <146130+ming-t18@users.noreply.github.com>
2025-09-20 00:42:44 -0700

    refactor(Simplicity): Split into multiple modules

---
+++ src/Data/Conway/Simplicity.hs
@@ -0,0 +1,16 @@
+module Data.Conway.Simplicity
+  ( ParentSeq,
+    ParentSeq0,
+    ConwaySeq (..),
+    MonoSeq (..),
+    Veb1Seq (..),
+    LeftRight (..),
+    parentDyadic,
+    parentConway,
+    parentMono,
+    parentVeb1,
+  )
+where
+
+import Data.Conway.Simplicity.Parent
+import Data.Conway.Simplicity.Types
