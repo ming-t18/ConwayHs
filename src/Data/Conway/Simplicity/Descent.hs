b49712721131ae6d543866b8aa2c14f475b4e794
Ming <146130+ming-t18@users.noreply.github.com>
2025-09-27 16:11:05 -0700

    feat(Simplicity): Descent sequence

--- src/Data/Conway/Simplicity/Descent.hs
+++ src/Data/Conway/Simplicity/Descent.hs
@@ -5,7 +5,7 @@ import qualified Data.Conway.Seq.InfList as I
 import Data.Conway.SignExpansion.Dyadic (FiniteSignExpansion)
 import Data.Conway.Simplicity.Completion (parentSeq)
 import Data.Conway.Simplicity.Seq
-import Data.Conway.Simplicity.Types (ConwaySeq (..), MonoSeq (..), ParentSeq, RangeElem (..), Veb1Seq (..))
+import Data.Conway.Simplicity.Types (ConwaySeq (..), MonoSeq (..), ParentSeq, RangeElem (..), Veb1Seq (..), psEmpty)
 import Data.Conway.Typeclasses
 import Data.List (unfoldr)
 
@@ -15,25 +15,20 @@ descend x =
     s <- parentSeq x
     case s of
       EPoint pt -> Just pt
-      ELimit s' ->
-        let cs = conwaySeq s'
-         in Just $ I.index cs $ decideIndex s'
-
-decideIndex :: ConwaySeq a -> Natural
-decideIndex (ConwaySeq {csTerm = t}) = decideIndexTerm t
-
-decideIndexTerm :: MonoSeq a -> Natural
-decideIndexTerm (Mono1Seq (Veb1ArgSeq _ a)) = decideIndex a
-decideIndexTerm (Mono1Seq (Veb1OrderSeq o _)) = decideIndex o
-decideIndexTerm (Mono1Seq (Veb1IterSeq _ Nothing)) = 3
-decideIndexTerm (Mono1Seq (Veb1IterSeq _ _)) = 1
-decideIndexTerm (MonoMultSeq _ True) = 1
-decideIndexTerm (MonoMultSeq _ False) = 0
+      ELimit s' -> Just $ I.index (conwaySeq s') 0
 
 descent :: (OrdRing a, FiniteSignExpansion a) => Conway a -> [Conway a]
 descent = unfoldr (fmap (\x -> (x, x)) . descend)
 
-descentSeq :: (OrdRing a, FiniteSignExpansion a) => Conway a -> [(ParentSeq a, Conway a)]
-descentSeq _ = error "TODO"
+descentSeq :: (OrdRing a, FiniteSignExpansion a) => Conway a -> [(Conway a, Bool, RangeElem a)]
+descentSeq x =
+  case s of
+    Nothing -> []
+    Just s'@(EPoint x') -> (x, x > x', s') : descentSeq x'
+    Just s'@(ELimit l) ->
+      let x' = I.index (conwaySeq l) 0
+       in (x, x > x', s') : descentSeq x'
+  where
+    s = parentSeq x
 
 -- TODO minimum exponent constraint on the sequence: w^(-3.5) + [w^-w][i] starts at w^(-3.5) + [w^-4] by skipping terms
