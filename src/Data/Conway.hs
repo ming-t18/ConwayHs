faf45336b00ea3d09cb5619f3372405d8c31a8e5
Ming <146130+ming-t18@users.noreply.github.com>
2025-10-26 13:03:50 -0700

    refactor: Add Conway top-level module for re-exports

---
+++ src/Data/Conway.hs
@@ -0,0 +1,8 @@
+module Data.Conway where
+
+import Data.Conway.Conway hiding (eps, mono1, veb1)
+import Data.Conway.Dyadic
+import Data.Conway.SignExpansion
+import Data.Conway.Simplicity
+import Data.Conway.Typeclasses
+import Data.Conway.Typeclasses.ConwayOps
